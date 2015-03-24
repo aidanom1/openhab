3bc05e91948d4ab0815929bfea32600d14692863
aidan <aidan.omahony1@mycit.ie>
2015-03-24 22:08:56 +0000

    Adding initial context code

---
+++ bundles/core/org.openhab.core/src/main/java/org/openhab/core/context/Context.java
@@ -0,0 +1,8 @@
+package org.openhab.core.context;
+
+public class Context {
+    private String Identity;
+    private String Activity;
+    private String Time;
+    private Location location;
+}
---
+++ bundles/core/org.openhab.core/src/main/java/org/openhab/core/context/Location.java
@@ -0,0 +1,5 @@
+package org.openhab.core.context;
+
+public class Location {
+
+}
